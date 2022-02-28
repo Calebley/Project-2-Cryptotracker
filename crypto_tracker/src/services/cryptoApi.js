7bb9792050d3c9d56b1969035ff68e2bb571aaaa
Caleb <calebley@hotmail.com>
2022-02-28 11:21:41 +0800

    creation of redux store

---
+++ crypto_tracker/src/services/cryptoApi.js
@@ -0,0 +1,16 @@
+import { }
+
+
+// fetch("https://coinranking1.p.rapidapi.com/exchanges?referenceCurrencyUuid=yhjMzLPhuIDl&limit=50&offset=0&orderBy=24hVolume&orderDirection=desc", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "coinranking1.p.rapidapi.com",
+// 		"x-rapidapi-key": "cf15e56a74mshb1b168ca64e28d4p16370bjsn33f812b21f78"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });

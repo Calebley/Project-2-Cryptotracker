1b3e8c8fb976c949ed3f96cfca7f2582e097096a
Caleb <calebley@hotmail.com>
2022-02-28 12:32:05 +0800

    created search input

--- crypto_tracker/src/components/Cryptocurrencies.jsx
+++ crypto_tracker/src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom"
 import { Card, Row, Col, Input } from "antd"
@@ -7,13 +7,24 @@ import { useGetCryptosQuery } from "../services/cryptoApi";
 const Cryptocurrencies = ({ simplified }) => {
     const count = simplified ? 10 : 100
     const { data: cryptoList, isFetching } = useGetCryptosQuery(count)
-    const [cryptos, setCryptos] = useState(cryptoList?.data?.coins)
+    const [cryptos, setCryptos] = useState([])
+    const [searchTerm, setSearchTerm] = useState("")
     console.log(cryptos)
 
-    if(isFetching) return "Loading..."
+    useEffect(() => {
+
+        const filteredData = cryptoList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+        setCryptos(filteredData)
+
+    }, [cryptoList, searchTerm])
+
+    if (isFetching) return "Loading..."
     return (
         <>
-            <Row gutter={[32,32]} className="crypto-card-container">
+            <div className="search-crypto">
+                <Input placeholder="Search coins" onChange={(e) => setSearchTerm(e.target.value)} />
+            </div>
+            <Row gutter={[32, 32]} className="crypto-card-container">
                 {cryptos?.map((currency) => (
                     <Col xs={24} className="crypto-card" key={currency.id}>
                         <Link to={`/crypto/${currency.id}`}>

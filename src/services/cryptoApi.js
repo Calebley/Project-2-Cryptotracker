5f6c89aec1299df3aaefce1379babe9982ffbaf8
Caleb <94058384+Calebley@users.noreply.github.com>
2022-03-15 20:52:13 +0800

    Update cryptoApi.js

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 const cryptoApiHeaders = {
-    "x-rapidapi-host": "coinranking1.p.rapidapi.com",
-    "x-rapidapi-key": "cf15e56a74mshb1b168ca64e28d4p16370bjsn33f812b21f78"
+
 }
 
 
